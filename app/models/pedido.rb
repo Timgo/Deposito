393a18fabcae78797d2e67e833c299d75f65249c
Timgo <tigomes@gmail.com>
2010-07-05 16:08:35 -0300

    mudando a referencia de pagamento para o Banco de Dados

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -1,16 +1,17 @@
 class Pedido < ActiveRecord::Base
   
-  TIPOS_DE_PAGAMENTO = [
+ # TIPOS_DE_PAGAMENTO = [
       #Exibidos             salvos no DB
-      ["Depósito Bancário", "banco" ],
-      ["Boleto Bancário",   "boleto" ],
-      ["PagSeguro",         "pagseguro" ],
-      ]
+  #    ["Depósito Bancário", "banco" ],
+   #   ["Boleto Bancário",   "boleto" ],
+    #  ["PagSeguro",         "pagseguro" ],
+    #  ]
 
      validates_presence_of :nome, :endereco, :email, :tipo_de_pagamento
-     validates_inclusion_of :tipo_de_pagamento, :in => TIPOS_DE_PAGAMENTO.map {|disp, value| value}
+     validates_inclusion_of :tipo_de_pagamento, :in => TipoDePagamento.find(:all).map {|tipo| tipo.registro}
      
    has_many :pedido_items
+   # :tipo_de_pagamento
    
   def adicionar_pedido_items_da_lista(lista)
     lista.items.each do |item|
